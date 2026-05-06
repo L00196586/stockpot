8ff038e0b602bc9c68069cfa3b6565cfef197e0d
Gonzalo Celina <249808122+L00196586@users.noreply.github.com>
2026-05-06 19:42:24 +0100

    Changes on Django admin

--- pantry/admin.py
+++ pantry/admin.py
@@ -4,14 +4,14 @@ from .models import Ingredient, StockItem
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
-    list_display = ["name", "unit"]
+    list_display = ["name"]
     search_fields = ["name"]
     ordering = ["name"]
 
 
 @admin.register(StockItem)
 class StockItemAdmin(admin.ModelAdmin):
-    list_display = ["user", "ingredient", "quantity", "expiry_date", "updated_at"]
-    list_filter = ["ingredient__unit", "expiry_date"]
+    list_display = ["user", "ingredient", "unit", "quantity", "expiry_date", "updated_at"]
+    list_filter = ["unit", "expiry_date"]
     search_fields = ["user__username", "ingredient__name"]
     ordering = ["user", "ingredient__name"]
